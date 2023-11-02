c965f8ab86ca4b823842692e6a50043bd9d1e1d9
Hemant2335 <knrt73373@gmail.com>
2023-11-02 21:45:08 +0530

    First_TypeScript_Setup_done

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Homepage from './Pages/Homepage'
+
+const App = () => {
+  return (
+    <div>
+      <></>
+      <Homepage/>
+    </div>
+  )
+}
+
+export default App
